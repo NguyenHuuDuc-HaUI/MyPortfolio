af4f209a845efa64f80d2f405d0d7d2fc6f75f0f
Nguyen Duc <nguyenduc302192@gmail.com>
2026-04-17 01:53:35 +0700

    Create project

---
+++ Backend/MyPortfolio/MyPortfolio.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MyPortfolio.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Frontend/my-portfolio/src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <div class="flex flex-col justify-around items-start p-8 h-72 mt-auto">
+      <h1 class="font-bold text-3xl underline justify-start mr-6">This is an about page</h1>
+      <p class="text-lg text-gray-600 mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
